6a2181dbea4c5b4e9375a3719b32b7a5587a0ff6
jrx <jerome+git@rouaix.eu>
2021-11-03 22:13:56 +0100

    yaml_to_json_number_tests

--- codegenr/src/loader.rs
+++ codegenr/src/loader.rs
@@ -21,7 +21,7 @@ fn yaml_to_json(yaml: serde_yaml::Value) -> Result<Value, anyhow::Error> {
   Ok(match yaml {
     Null => Value::Null,
     Bool(b) => Value::Bool(b),
-    Number(n) => Value::Number(yaml_to_json_number(n)),
+    Number(n) => Value::Number(yaml_to_json_number(n)?),
     String(s) => Value::String(s),
     Sequence(values) => Value::Array(values.into_iter().map(yaml_to_json).collect::<Result<Vec<_>, _>>()?),
     Mapping(map) => {
@@ -38,8 +38,21 @@ fn yaml_to_json(yaml: serde_yaml::Value) -> Result<Value, anyhow::Error> {
   })
 }
 
-fn yaml_to_json_number(_number: serde_yaml::Number) -> serde_json::Number {
-  todo!()
+fn yaml_to_json_number(n: serde_yaml::Number) -> Result<serde_json::Number, anyhow::Error> {
+  use serde_json::Number;
+  let number = if n.is_f64() {
+    let f = n.as_f64().ok_or_else(|| anyhow::format_err!("The number should be an f64."))?;
+    Number::from_f64(f).ok_or_else(|| anyhow::format_err!("The number couldn't map to json."))?
+  } else if n.is_u64() {
+    let u = n.as_u64().ok_or_else(|| anyhow::format_err!("The number should be an u64."))?;
+    Number::from(u)
+  } else if n.is_i64() {
+    let u = n.as_i64().ok_or_else(|| anyhow::format_err!("The number should be an i64."))?;
+    Number::from(u)
+  } else {
+    return Err(anyhow::anyhow!("There is a new number flavor in yaml ?"));
+  };
+  Ok(number)
 }
 
 #[cfg(test)]
@@ -66,6 +79,7 @@ mod test {
     assert_eq!(yaml_to_json(Null)?, Value::Null);
     assert_eq!(yaml_to_json(Bool(true))?, Value::Bool(true));
     assert_eq!(yaml_to_json(Bool(false))?, Value::Bool(false));
+
     // todo String / Number
 
     assert_eq!(
@@ -82,6 +96,19 @@ mod test {
 
     // Todo : fail when map key is not string
     // yaml_to_json(Null).map_err(|e| e.to_string());
+
+    Ok(())
+  }
+
+  #[test]
+  fn yaml_to_json_number_tests() -> Result<(), anyhow::Error> {
+    use serde_yaml::Number;
+
+    let expected_failed = yaml_to_json_number(Number::from(f64::INFINITY));
+    let e = expected_failed.expect_err("Should be an error");
+    assert_eq!(e.to_string(), "The number couldn't map to json.");
+
+    // todo : ok tests with  Number::from() i64, u64, and non weird f64 ... and some negative values
 
     Ok(())
   }
