85af4ce03919a152dc5f329e1f0dab258057f58b
jrx <jerome+git@rouaix.eu>
2021-11-04 23:08:27 +0100

    WIP

--- codegenr/src/resolver.rs
+++ codegenr/src/resolver.rs
@@ -78,6 +78,7 @@ mod test {
   use super::*;
   use crate::loader::*;
   use serde_json::json;
+  use test_case::test_case;
 
   #[test]
   fn resolve_reference_test() -> Result<(), anyhow::Error> {
@@ -343,4 +344,67 @@ mod test {
     assert!(!string.contains(REF));
     Ok(())
   }
+
+  // #[test_case("_yamlSamples/petshop.yaml", "../test.json", false, true, "test.json", "")]
+  // #[test_case("_yamlSamples/petshop.yaml", "test.json", false, true, "_yamlSamples/test.json", "")]
+  // #[test_case("_yamlSamples/petshop.yaml", "#test", true, true, "_yamlSamples/petshop.yaml", "test")]
+  // #[test_case("_yamlSamples/petshop.yaml", "test.json#test", false, true, "_yamlSamples/test.json", "test")]
+  // #[test_case("_yamlSamples/petshop.yaml", "http://google.com/test.json#test", false, false, "http://google.com/test.json", "test")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "#/definitions/Pet", true, false, "https://petstore.swagger.io/v2/swagger.json", "/definitions/Pet")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "http://google.com/test.json#test", false, false, "http://google.com/test.json", "test")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "http://google.com/test.json", false, false, "http://google.com/test.json", "")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "../test.json", false, false, "https://petstore.swagger.io/test.json", "")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "../test.json#fragment", false, false, "https://petstore.swagger.io/test.json", "fragment")]
+
+  // public void GetRefInfo(string document, string @ref, bool expectedIsNested, bool expectedIsLocal, string expectedUri, string expectedPath)
+
+  fn multiplication_tests(current_doc: &str, ref_path: &str, expected_is_nested: bool) {
+    let ref_info = RefInfo::parse(ref_path);
+    // assert_eq!(ref_info.is_nested, expected_is_nested);
+    todo!();
+    // assert_eq!(ref_info.is_nested, expected_is_nested);
+  }
+
+  struct RefInfo {
+    /// True if the reference is nested in the same document
+    pub is_nested: bool,
+    // public bool IsNestedInThisDocument { get; }
+    // public bool IsLocal { get; }
+    // public Uri AbsoluteDocumentUri { get; }
+    // public string InDocumentPath { get; }
+    // public bool IsFalseAbsoluteRef { get; }
+    // public string RefFriendlyName { get; }
+  }
+
+  impl RefInfo {
+    pub fn parse(ref_path: &str) -> Self {
+      todo!()
+    }
+  }
 }
+
+/*
+  [Theory]
+
+        public void GetRefInfo(string document, string @ref, bool expectedIsNested, bool expectedIsLocal, string expectedUri, string expectedPath)
+        {
+            var refInfo = RefInfo.GetRefInfo(document, @ref);
+
+            if (!new Uri(expectedUri, UriKind.RelativeOrAbsolute).IsAbsoluteUri)
+            {
+                expectedUri = Path.Combine(Directory.GetCurrentDirectory(), expectedUri);
+                expectedUri = new Uri(Path.GetFullPath(expectedUri)).AbsoluteUri.ToString();
+            }
+            else
+            {
+                expectedUri = new Uri(expectedUri).AbsoluteUri.ToString();
+            }
+
+            var absoluteUri = refInfo.AbsoluteDocumentUri.AbsoluteUri.ToString();
+
+            refInfo.IsNestedInThisDocument.ShouldBe(expectedIsNested);
+            refInfo.IsLocal.ShouldBe(expectedIsLocal);
+            absoluteUri.ShouldBe(expectedUri);
+            refInfo.InDocumentPath.ShouldBe(expectedPath);
+        }
+*/
