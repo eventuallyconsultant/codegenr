83132b6905f7339510d69b6e08b37f04b2143c58
jrx <jerome+git@rouaix.eu>
2021-11-05 11:29:37 +0100

    transformer module

--- codegenr/src/main.rs
+++ codegenr/src/main.rs
@@ -1,6 +1,7 @@
-pub mod resolver;
 pub mod loader;
+pub mod resolver;
+pub mod transformer;
 
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
 }
---
+++ codegenr/src/transformer.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn some_test() -> Result<(), anyhow::Error> {
+    todo!()
+  }
+}
