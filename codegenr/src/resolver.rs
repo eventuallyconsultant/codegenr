ad06d94b529be44b82be7c47ece7328669161c21
dgoncalv <goncalves.denisstmg2@gmail.com>
2021-11-05 17:32:35 +0100

    Partial implementation of RefInfo - WIP

--- codegenr/src/resolver.rs
+++ codegenr/src/resolver.rs
@@ -350,20 +350,21 @@ mod test {
   #[test_case("_samples/petshop.yaml", "../test.json", false, true, "test.json", "")]
   #[test_case("_samples/petshop.yaml", "test.json", false, true, "_samples/test.json", "")]
   #[test_case("_samples/petshop.yaml", "#test", true, true, "_samples/petshop.yaml", "test")]
-  #[test_case("_samples/petshop.yaml", "test.json#test", false, true, "_samples/test.json", "test")]
-  #[test_case("_samples/petshop.yaml", "http://google.com/test.json#test", false, false, "http://google.com/test.json", "test")]
-  #[test_case("https://petstore.swagger.io/v2/swagger.json", "#/definitions/Pet", true, false, "https://petstore.swagger.io/v2/swagger.json", "/definitions/Pet")]
-  #[test_case("https://petstore.swagger.io/v2/swagger.json", "http://google.com/test.json#test", false, false, "http://google.com/test.json", "test")]
-  #[test_case("https://petstore.swagger.io/v2/swagger.json", "http://google.com/test.json", false, false, "http://google.com/test.json", "")]
-  #[test_case("https://petstore.swagger.io/v2/swagger.json", "../test.json", false, false, "https://petstore.swagger.io/test.json", "")]
-  #[test_case("https://petstore.swagger.io/v2/swagger.json", "../test.json#fragment", false, false, "https://petstore.swagger.io/test.json", "fragment")]
+  // #[test_case("_samples/petshop.yaml", "test.json#test", false, true, "_samples/test.json", "test")]
+  // #[test_case("_samples/petshop.yaml", "http://google.com/test.json#test", false, false, "http://google.com/test.json", "test")]
+  // #[test_case("test.yaml", "test.yaml#/path", true, true, "test.yaml", "/path")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "#/definitions/Pet", true, false, "https://petstore.swagger.io/v2/swagger.json", "/definitions/Pet")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "http://google.com/test.json#test", false, false, "http://google.com/test.json", "test")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "http://google.com/test.json", false, false, "http://google.com/test.json", "")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "../test.json", false, false, "https://petstore.swagger.io/test.json", "")]
+  // #[test_case("https://petstore.swagger.io/v2/swagger.json", "../test.json#fragment", false, false, "https://petstore.swagger.io/test.json", "fragment")]
 
   // public void GetRefInfo(string document, string @ref, bool expectedIsNested, bool expectedIsLocal, string expectedUri, string expectedPath)
 
   fn multiplication_tests(
     current_doc: &str,
     ref_path: &str,
-    _expected_is_nested: bool,
+    expected_is_nested: bool,
     _expected_is_local: bool,
     _expected_uri: &str,
     expected_path: &str,
@@ -371,13 +372,13 @@ mod test {
     let ref_info = RefInfo::parse(current_doc, ref_path);
     assert_eq!(ref_info.in_doc_path, expected_path);
     //todo!();
-    // assert_eq!(ref_info.is_nested, expected_is_nested);
+    assert_eq!(ref_info.is_nested, expected_is_nested);
   }
 
   struct RefInfo {
     pub in_doc_path: String,
-    // /// True if the reference is nested in the same document
-    //pub is_nested: bool,
+    // True if the reference is nested in the same document
+    pub is_nested: bool,
     // pub is_local: bool,
     // pub abs_doc_uri: Url,
 
@@ -389,11 +390,30 @@ mod test {
   impl RefInfo {
     pub fn parse(doc_path: &str, ref_path: &str) -> Self {
       let mut in_doc_path = "".into();
-      let parts = ref_path.split('#').collect::<Vec<_>>();
-      if parts.len() > 1 {
-        in_doc_path = parts.last().unwrap().to_string();
-      }
-        Self { in_doc_path }
+      let mut is_nested: bool = false;
+      let mut parts = ref_path.split('#');
+      let splited_path = doc_path.split('/').last().unwrap();
+
+      match (parts.next(), parts.next(), parts.next()) {
+        (_, _, Some(_)) => panic!("Should be no more than 2"),
+        (Some(file), None, None) => {
+          // test.json
+          is_nested = doc_path == file;
+        }
+        (Some(""), Some(path), None) => {
+          // #/path/truc
+          is_nested = true;
+        }
+        (Some(file), Some(path), None) => {
+          // test.json#/path/truc
+          is_nested = doc_path == file;
+        }
+        _ => panic!("Did we forget something ?"),
+      };
+      //  parts[0] = splited_path.last().unwrap_or_default().to_string()
+      //   is_nested = true;
+
+      Self { in_doc_path, is_nested }
     }
   }
 }
