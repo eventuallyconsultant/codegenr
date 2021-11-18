da161f474fd1a8745b085e3e23a59b0afa25d121
jrx <jerome+git@rouaix.eu>
2021-11-18 22:42:40 +0100

    get_templates_from_directory_test

--- codegenr/src/render.rs
+++ codegenr/src/render.rs
@@ -13,16 +13,37 @@ pub enum TemplateType {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Template {
-  pub template_type: TemplateType,
-  pub file_name: String,
-  pub file_path: String,
+  template_type: TemplateType,
+  file_name: String,
+  file_path: String,
+}
+
+impl Template {
+  pub fn new(template_type: TemplateType, file_name: impl Into<String>, file_path: impl Into<String>) -> Self {
+    Self {
+      template_type,
+      file_name: file_name.into(),
+      file_path: file_path.into(),
+    }
+  }
+
+  pub fn file_path(&self) -> &str {
+    &self.file_path
+  }
+
+  pub fn template_name(&self) -> &str {
+    self.file_name.trim_start_matches('_').trim_end_matches(".hbs")
+  }
+
+  pub fn template_type(&self) -> TemplateType {
+    self.template_type
+  }
 }
 
 pub fn get_templates_from_directory(dir_path: &str) -> Result<Vec<Template>, anyhow::Error> {
   let mut result = vec![];
   for entry in WalkDir::new(dir_path) {
     let entry = entry?;
-    dbg!(&entry);
 
     let file_path = entry.path().to_str();
     let file_path = match file_path {
@@ -31,19 +52,16 @@ pub fn get_templates_from_directory(dir_path: &str) -> Result<Vec<Template>, any
     };
 
     let file_name = entry.file_name().to_str();
-    if let Some(f_name) = file_name {
-      if !f_name.ends_with(HANDLEBARS_TEMPLATE_EXTENSION) {
+    if let Some(file_name) = file_name {
+      if !file_name.ends_with(HANDLEBARS_TEMPLATE_EXTENSION) {
         continue;
       }
-      result.push(Template {
-        file_name: f_name.to_string(),
-        file_path: file_path.to_string(),
-        template_type: if f_name.starts_with(PARTIAL_TEMPLATE_PREFIX) {
-          TemplateType::Partial
-        } else {
-          TemplateType::Main
-        },
-      })
+      let t = if file_name.starts_with(PARTIAL_TEMPLATE_PREFIX) {
+        TemplateType::Partial
+      } else {
+        TemplateType::Main
+      };
+      result.push(Template::new(t, file_name, file_path));
     }
   }
   Ok(result)
@@ -70,14 +88,22 @@ mod test {
   fn get_templates_from_directory_test() -> Result<(), anyhow::Error> {
     let mut templates = get_templates_from_directory("./_samples/render/templates")?;
     templates.sort_by_key(|t| t.file_path.clone());
-    assert_eq!(
-      templates,
-      vec!(Template {
-        template_type: TemplateType::Main,
-        file_name: "".into(),
-        file_path: Default::default(),
-      })
-    );
+    let expected = vec![
+      Template::new(
+        TemplateType::Partial,
+        "_other_partial.hbs",
+        "./_samples/render/templates/_other_partial.hbs",
+      ),
+      Template::new(TemplateType::Partial, "_partial.hbs", "./_samples/render/templates/_partial.hbs"),
+      Template::new(TemplateType::Partial, "_plop.hbs", "./_samples/render/templates/sub/_plop.hbs"),
+      Template::new(TemplateType::Main, "plop.hbs", "./_samples/render/templates/sub/plop.hbs"),
+      Template::new(TemplateType::Main, "test.hbs", "./_samples/render/templates/test.hbs"),
+    ];
+    // dbg!(&templates, &expected);
+    assert_eq!(templates, expected);
+
+    let first = templates.get(0).expect("?");
+    assert_eq!(first.template_name(), "other_partial");
     Ok(())
   }
 }
