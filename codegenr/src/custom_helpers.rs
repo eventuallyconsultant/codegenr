56a2b9d2116aac79c5e4b1773e624bc0d3e1e1a3
jrx <jerome+git@rouaix.eu>
2021-11-24 22:18:22 +0100

    some doc test exemple for helpers

--- codegenr/src/custom_helpers.rs
+++ codegenr/src/custom_helpers.rs
@@ -1,7 +1,32 @@
 use handlebars::handlebars_helper;
 use handlebars::Handlebars;
 
-handlebars_helper!(hex: |v: i64| format!("0x{:x}", v));
+pub fn register_custom_helpers(handlebars: &mut Handlebars) {
+  handlebars.register_helper("hex", Box::new(Hex));
+  handlebars.register_helper("trim", Box::new(trim));
+  handlebars.register_helper("UppercaseFirstLetter", Box::new(UppercaseFirstLetter));
+  handlebars.register_helper("ToLowerCase", Box::new(ToLowerCase));
+  handlebars.register_helper("StartWith", Box::new(StartWith));
+}
+
+/// Return the hexadecimal representation of the integer value
+/// ```
+/// # use codegenr::custom_helpers::*;
+/// # use serde_json::json;
+/// assert_eq!(
+///   test_helper(serde_json::Value::Null, "{{hex 42}}"),
+///   "0x2a"
+/// );
+/// assert_eq!(
+///   test_helper(json!({ "value": 42 }), "{{hex value}}"),
+///   "0x2a"
+/// );
+/// ```
+pub fn hex(v: i64) -> String {
+  format!("0x{:x}", v)
+}
+
+handlebars_helper!(Hex: |v: i64| hex(v));
 
 handlebars_helper!(trim: |v: String| v.trim());
 
@@ -32,17 +57,10 @@ pub fn captitalize_first_letter(v: String) -> String {
   result
 }
 
-pub fn register_custom_helpers(handlebars: &mut Handlebars) {
-  handlebars.register_helper("hex", Box::new(hex));
-  handlebars.register_helper("trim", Box::new(trim));
-  handlebars.register_helper("UppercaseFirstLetter", Box::new(UppercaseFirstLetter));
-  handlebars.register_helper("ToLowerCase", Box::new(ToLowerCase));
-  handlebars.register_helper("StartWith", Box::new(StartWith));
+// #[cfg(doctest)]
+pub fn test_helper(json: serde_json::Value, template: &str) -> String {
+  let mut h = Handlebars::new();
+  register_custom_helpers(&mut h);
+  h.register_template_string("test", template).expect("OK!");
+  h.render("test", &json).unwrap()
 }
-
-/// # Examples
-///
-/// ```
-/// let x = 5;
-/// ```
-fn test() {}
