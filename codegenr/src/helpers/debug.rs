08c35c1e32027926c85c82279a91d94890883eb6
jrx <jerome+git@rouaix.eu>
2021-12-09 22:11:17 +0100

    debug helpers test changes

--- codegenr/src/helpers/debug.rs
+++ codegenr/src/helpers/debug.rs
@@ -1,4 +1,4 @@
-use handlebars::{HelperDef, ScopedJson};
+use handlebars::HelperDef;
 use serde_json::{json, Value};
 
 pub const DEBUG: &str = "debug";
@@ -10,7 +10,26 @@ pub const DEBUG_CTX: &str = "debug_ctx";
 /// # use serde_json::json;
 /// assert_eq!(
 ///   exec_template(json!({"plop": "plop"}), "{{debug 42 \"42\" plop non_existing}}"),
-///   r#"[{"relative_path":"","value":42},{"relative_path":"","value":"42"},{"relative_path":"plop","value":"plop"},{"relative_path":"non_existing","value":null}]"#
+///   r#"
+/// [
+///   {
+///     "relative_path": "",
+///     "value": 42
+///   },
+///   {
+///     "relative_path": "",
+///     "value": "42"
+///   },
+///   {
+///     "relative_path": "plop",
+///     "value": "plop"
+///   },
+///   {
+///     "relative_path": "non_existing",
+///     "value": null
+///   }
+/// ]
+/// "#
 /// );
 /// ```
 pub struct DebugHelper;
@@ -47,10 +66,22 @@ impl HelperDef for DebugHelper {
 /// ```
 /// # use codegenr::helpers::*;
 /// # use serde_json::json;
-/// assert_eq!(
-///   exec_template(json!({"plop": "plop"}), "{{debug_ctx 42 \"42\" plop non_existing}}"),
-///   ""
-/// );
+///
+/// let _result = exec_template(json!({"plop": "plop"}), "{{debug_ctx 42 \"42\" plop non_existing}}");
+///
+/// // Couldn't have this test pass deterministically, but the result should be close to this
+/// // assert_eq!(
+/// //   _result,
+/// //   r#"helper:
+/// // Helper { name: "debug_ctx", params: [PathAndJson { relative_path: None, value: Constant(Number(42)) }, PathAndJson { relative_path: None, value: Constant(String("42")) }, PathAndJson { relative_path: Some("plop"), value: Context(String("plop"), ["plop"]) }, PathAndJson { relative_path: Some("non_existing"), value: Missing }], hash: {}, template: None, inverse: None, block_param: None, block: false }
+/// // handlebars:
+/// // Handlebars { templates: {"test": Template { name: Some("test"), elements: [Expression(HelperTemplate { name: Name("debug_ctx"), params: [Literal(Number(42)), Literal(String("42")), Path(Relative(([Named("plop")], "plop"))), Path(Relative(([Named("non_existing")], "non_existing")))], hash: {}, block_param: None, template: None, inverse: None, block: false })], mapping: [TemplateMapping(1, 1)] }}, helpers: ["lower_case", "trim_end", "split_get_last", "not", "start_with", "trim_block_end", "trim", "one_line", "if_array_contains", "no_empty_lines", "gte", "with", "lookup", "debug_ctx", "with_matching", "lte", "trim_start", "set", "with_set", "regex_extract", "if", "if_empty", "if_set", "trim_block_start", "log", "trim_block", "global_parameter", "gt", "upper_case", "len", "pascal_case", "ne", "debug", "camel_case", "clear", "lowercase_first_letter", "or", "unless", "if_equals", "get", "if_not_empty", "if_not_equals", "snake_case", "split_get_first", "hex", "uppercase_first_letter", "each", "and", "raw", "eq", "lt"], decorators: ["inline"], strict_mode: false, dev_mode: true }
+/// // context:
+/// // Context { data: Object({"plop": String("plop")}) }
+/// // render_context:
+/// // RenderContext { inner: RenderContextInner { partials: {}, partial_block_stack: [], root_template: Some("test"), current_template: Some("test"), disable_eacape: false }, blocks: [BlockContext { base_path: [], base_value: None, block_params: BlockParams { data: {} }, local_variables: LocalVars { first: None, last: None, index: None, key: None, extra: {} } }], modified_context: None }
+/// // "#
+/// //);
 /// ```
 pub struct DebugCtxHelper;
 
