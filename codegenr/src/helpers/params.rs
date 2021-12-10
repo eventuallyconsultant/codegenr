b112501d7af40801048e0fa94387a85a1a21414d
jrx <jerome+git@rouaix.eu>
2021-12-10 23:03:21 +0100

    permissive global_parameter helper

--- codegenr/src/helpers/params.rs
+++ codegenr/src/helpers/params.rs
@@ -1,12 +1,12 @@
 use super::handlebars_ext::HandlebarsExt;
-use handlebars::{HelperDef, RenderError};
+use handlebars::{HelperDef, RenderError, ScopedJson};
 use serde_json::Value;
 use std::collections::HashMap;
 
 pub const GLOBAL_PARAMETERS_HELPER: &str = "global_parameter";
 
 /// Gets a value from the global parameters
-/// (those key/value's would be provided as parameters of `codegenr` execution)
+/// (those key=value's would be provided as parameters of `codegenr` execution)
 /// ```
 /// # use codegenr::helpers::*;
 /// # use serde_json::json;
@@ -15,16 +15,20 @@ pub const GLOBAL_PARAMETERS_HELPER: &str = "global_parameter";
 /// params.insert("k".to_string(), json!("v"));
 ///
 /// assert_eq!(
-///   exec_template_with_global_params(json!({}), r#"{{global_parameter "k"}}"#, params),
+///   exec_template_with_global_params(json!({}), r#"{{global_parameter "k"}}"#, params.clone()),
 ///   "v"
+/// );
+/// assert_eq!(
+///   exec_template_with_global_params(json!({}), r#"{{global_parameter "non_existing"}}"#, params),
+///   ""
 /// );
 /// ```
 ///
-/// An error will be raise if a non existing key is asked
+/// An error will be raise if a non existing key is asked and second parameter is true
 /// ```should_panic
 /// # use serde_json::json;
 /// # use codegenr::helpers::*;
-/// exec_template_with_global_params(json!({}), r#"{{global_parameter "k"}}"#, Default::default());
+/// exec_template_with_global_params(json!({}), r#"{{global_parameter "k" true}}"#, Default::default());
 /// ```
 pub struct GlobalParameterHelper {
   values: HashMap<String, Value>,
@@ -43,18 +47,24 @@ impl HelperDef for GlobalParameterHelper {
     _: &'reg handlebars::Handlebars<'reg>,
     _: &'rc handlebars::Context,
     _: &mut handlebars::RenderContext<'reg, 'rc>,
-  ) -> Result<handlebars::ScopedJson<'reg, 'rc>, RenderError> {
-    h.ensure_arguments_count(1, GLOBAL_PARAMETERS_HELPER)?;
+  ) -> Result<ScopedJson<'reg, 'rc>, RenderError> {
+    h.ensure_arguments_count_min(1, GLOBAL_PARAMETERS_HELPER)?;
+    h.ensure_arguments_count_max(2, GLOBAL_PARAMETERS_HELPER)?;
     let key = h.get_param_as_str_or_fail(0, GLOBAL_PARAMETERS_HELPER)?.to_string();
-    self
-      .values
-      .get(&key)
-      .map(|v| handlebars::ScopedJson::Derived(v.clone()))
-      .ok_or_else(|| {
-        RenderError::new(format!(
-          "{}, error: The key or the associated value to itself doesn't exist.",
-          GLOBAL_PARAMETERS_HELPER
-        ))
-      })
+
+    match self.values.get(&key).cloned() {
+      Some(v) => Ok(ScopedJson::Derived(v)),
+      None => {
+        let strict_mode = h.get_param_as_bool(1).unwrap_or(false);
+        if strict_mode {
+          Err(RenderError::new(format!(
+            "`{}`, error: Cannot find a value for key `{}`",
+            GLOBAL_PARAMETERS_HELPER, key
+          )))
+        } else {
+          Ok(ScopedJson::Derived(Default::default()))
+        }
+      }
+    }
   }
 }
