ea0ee821a285666f552a0b637271e20c4d3e809f
jrx <jerome+git@rouaix.eu>
2021-12-14 10:14:00 +0100

    codegenr commands file|gen

--- codegenr/src/main.rs
+++ codegenr/src/main.rs
@@ -1,81 +1,24 @@
+use std::collections::HashMap;
+
 use codegenr_lib::{run_codegenr, Options};
-use serde_json::Value;
+use opt::Opt;
 use structopt::StructOpt;
 
-const CODEGENR_CONFIG_FILE: &str = "codegenr.toml";
+mod opt;
 
-//https://docs.rs/structopt/latest/structopt/#specifying-argument-types
-#[derive(StructOpt, Debug)]
-#[structopt(name = "codegenr")]
-struct Opt {
-  #[structopt(long, short, help = "Source json/yaml/openapi file.")]
-  pub source: String,
-  #[structopt(long, short, help = "Output folder.")]
-  pub output: String,
-  #[structopt(
-    long,
-    short,
-    help = "Templates folder(s), in which only one .hbs file should have no `_` as prefix (Underscored templates are partial templates)."
-  )]
-  pub templates: Vec<String>,
-  #[structopt(
-    long,
-    short,
-    help = "Optional path to a file where the intermediate json representation of resolved source(s) will be output. The resolved json will be output as <file>.resolved.json, the full text rendered result will be output as <file>.rendered.txt"
-  )]
-  pub intermediate: Option<String>,
-  #[structopt(long, short, help = "Path to custom helper files.")]
-  pub custom_helpers: Vec<String>,
-  #[structopt(
-    long,
-    short,
-    help = "Global parameters values formatted `key=value`. Values will be parsed as json or strings if the json parsing fails.",
-    parse(try_from_str = parse_key_val)
-  )]
-  pub global_parameters: Vec<(String, serde_json::Value)>,
-}
-
-// From here: https://github.com/TeXitoi/structopt/blob/master/examples/keyvalue.rs
-// Parse a single key-value pair
-fn parse_key_val(s: &str) -> Result<(String, Value), anyhow::Error> {
-  let pos = s
-    .find('=')
-    .ok_or_else(|| anyhow::anyhow!("Invalid key=value: no `=` found in `{}`.", s))?;
-  let value = &s[pos + 1..];
-  Ok((
-    s[..pos].parse()?,
-    value.parse().unwrap_or_else(|_| Value::String(value.to_string())),
-  ))
-}
-
-impl From<Opt> for Options {
-  fn from(opt: Opt) -> Self {
-    Self {
-      source: opt.source,
-      output: opt.output,
-      templates: opt.templates,
-      intermediate: opt.intermediate,
-      custom_helpers: opt.custom_helpers,
-      global_parameters: opt.global_parameters.into_iter().collect(),
-    }
-  }
-}
+pub const CODEGENR_CONFIG_FILE: &str = "codegenr.toml";
 
 fn main() -> Result<(), anyhow::Error> {
-  let codegen_file = std::fs::read_to_string(CODEGENR_CONFIG_FILE);
-  let options = Opt::from_args_safe();
-  match (codegen_file, options) {
-    (Ok(_ignored_config_file), Ok(options)) => run_codegenr(options.into())?,
-    (Ok(config_file), Err(_)) => {
-      let opts: std::collections::HashMap<String, Options> = toml::from_str(&config_file)?;
-      for (name, options) in opts {
-        if let Err(e) = run_codegenr(options) {
-          println!("Error while executing the `{}` section: `{}`", name, e);
-        }
-      }
+  let options = Opt::from_args();
+
+  let cmd = options.cmd.unwrap_or_default();
+  let options_map: HashMap<String, Options> = cmd.try_into()?;
+
+  for (name, options) in options_map {
+    if let Err(e) = run_codegenr(options) {
+      println!("Error while executing the `{}` section: `{}`.", name, e);
     }
-    (Err(_), Ok(options)) => run_codegenr(options.into())?,
-    (Err(_), Err(clap_error)) => clap_error.exit(),
   }
+
   Ok(())
 }
---
+++ codegenr/src/opt.rs
@@ -0,0 +1,117 @@
+use codegenr_lib::Options;
+use serde_json::Value;
+use std::{collections::HashMap, fs::read_to_string};
+use structopt::StructOpt;
+
+//https://docs.rs/structopt/latest/structopt/#specifying-argument-types
+#[derive(StructOpt, Debug)]
+#[structopt(name = "codegenr")]
+pub struct Opt {
+  // todo later
+  // #[structopt(
+  //   long,
+  //   short,
+  //   help = "If set, codegenr keeps running and watch for all concerned files change. And then re-run the generation.",
+  //   global = true
+  // )]
+  // pub watch: bool,
+  #[structopt(subcommand)]
+  pub cmd: Option<Command>,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "Commands")]
+pub enum Command {
+  #[structopt(name = "file", help = "Executes all generations from a config file.")]
+  FromFile {
+    #[structopt(
+        long,
+        help = "Path to a full codegenr configuration file. If existing, all other command line parameters are ignored",
+        default_value = crate::CODEGENR_CONFIG_FILE
+      )]
+    file: String,
+  },
+  #[structopt(name = "gen", help = "Executes one generation from command line parameters.")]
+  FromLine {
+    #[structopt(long, short, help = "Source json/yaml/openapi file.")]
+    source: String,
+    #[structopt(long, short, help = "Output folder.")]
+    output: String,
+    #[structopt(
+      long,
+      short,
+      help = "Templates folder(s), in which only one .hbs file should have no `_` as prefix (Underscored templates are partial templates)."
+    )]
+    templates: Vec<String>,
+    #[structopt(
+      long,
+      short,
+      help = "Optional path to a file where the intermediate json representation of resolved source(s) will be output. 
+        The resolved json will be output as <file>.resolved.json, the full text rendered result will be output as <file>.rendered.txt."
+    )]
+    intermediate: Option<String>,
+    #[structopt(long, short, help = "Path to custom helper files.")]
+    custom_helpers: Vec<String>,
+    #[structopt(
+        long,
+        short,
+        help = "Global parameters values formatted `key=value`. Values will be parsed as json or strings if the json parsing fails.",
+        parse(try_from_str = parse_key_val)
+      )]
+    global_parameters: Vec<(String, serde_json::Value)>,
+  },
+}
+
+impl Default for Command {
+  fn default() -> Self {
+    Self::FromFile {
+      file: crate::CODEGENR_CONFIG_FILE.into(),
+    }
+  }
+}
+
+// From here: https://github.com/TeXitoi/structopt/blob/master/examples/keyvalue.rs
+// Parse a single key-value pair
+fn parse_key_val(s: &str) -> Result<(String, Value), anyhow::Error> {
+  let pos = s
+    .find('=')
+    .ok_or_else(|| anyhow::anyhow!("Invalid key=value: no `=` found in `{}`.", s))?;
+  let value = &s[pos + 1..];
+  Ok((
+    s[..pos].parse()?,
+    value.parse().unwrap_or_else(|_| Value::String(value.to_string())),
+  ))
+}
+
+impl TryFrom<Command> for HashMap<String, Options> {
+  type Error = anyhow::Error;
+
+  fn try_from(cmd: Command) -> Result<Self, Self::Error> {
+    match cmd {
+      Command::FromFile { file } => {
+        let config = read_to_string(file)?;
+        let opts: HashMap<String, Options> = toml::from_str(&config)?;
+        Ok(opts)
+      }
+      Command::FromLine {
+        source,
+        output,
+        templates,
+        intermediate,
+        custom_helpers,
+        global_parameters,
+      } => {
+        let options = Options {
+          source,
+          output,
+          templates,
+          intermediate,
+          custom_helpers,
+          global_parameters: global_parameters.into_iter().collect(),
+        };
+        let map = HashMap::<String, Options>::from_iter(std::iter::once(("command_line".into(), options)));
+        Ok(map)
+      }
+    }
+  }
+}
