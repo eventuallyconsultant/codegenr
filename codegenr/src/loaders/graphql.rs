69443310b9e2ac0db58d7c12f03108f85de38055
jrx <jerome+git@rouaix.eu>
2022-02-13 21:28:54 +0100

    graphql to something jsonable

--- codegenr/src/loaders/graphql.rs
+++ codegenr/src/loaders/graphql.rs
@@ -1,17 +1,18 @@
 use graphql_parser::schema::*;
+use serde::Serialize;
 use serde_json::{Map, Value};
 
 use super::LoaderError;
 
 pub type Directives = Vec<GraphqlDirective>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct Graphql {
   pub schema: GraphqlSchema,
   pub definitions: Vec<GraphqlDefinition>,
 }
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, Serialize)]
 pub struct GraphqlSchema {
   pub query: Option<String>,
   pub mutation: Option<String>,
@@ -19,7 +20,7 @@ pub struct GraphqlSchema {
   pub directives: Directives,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct GraphqlDirective {
   pub name: String,
   pub arguments: Vec<(String, Value)>,
@@ -43,12 +44,121 @@ fn graphql_directives_to_object(graphql: Vec<Directive<'_, String>>) -> Result<D
   graphql.into_iter().map(|d| d.try_into()).collect::<Result<Directives, _>>()
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct GraphqlDefinition {
+  #[serde(rename = "type")]
   pub definition_type: GraphqlDefinitionType,
+  pub name: String,
+  pub description: Option<String>,
+  pub directives: Directives,
+  #[serde(rename = "implementsInterfaces")]
+  pub implements_interfaces: Vec<String>,
+  pub fields: Vec<GraphqlField>,
+  pub inputs: Vec<GraphqlInputValue>,
+  #[serde(rename = "unionOf")]
+  pub union_of: Vec<String>,
+  pub values: Vec<GraphqlEnumValue>,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone, Serialize)]
+pub struct GraphqlField {
+  pub name: String,
+  pub description: Option<String>,
+  pub arguments: Vec<GraphqlInputValue>,
+  #[serde(rename = "fieldType")]
+  pub field_type: GraphqlType,
+  pub directives: Directives,
+}
+
+impl<'a> TryFrom<Field<'a, String>> for GraphqlField {
+  type Error = LoaderError;
+  fn try_from(field: Field<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
+      name: field.name,
+      description: field.description,
+      arguments: field.arguments.into_iter().map(|a| a.try_into()).collect::<Result<Vec<_>, _>>()?,
+      field_type: field.field_type.try_into()?,
+      directives: graphql_directives_to_object(field.directives)?,
+    })
+  }
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct GraphqlInputValue {
+  pub name: String,
+  pub description: Option<String>,
+  #[serde(rename = "valueType")]
+  pub value_type: GraphqlType,
+  #[serde(rename = "defaultValue")]
+  pub default_value: Option<Value>,
+  pub directives: Directives,
+}
+
+impl<'a> TryFrom<InputValue<'a, String>> for GraphqlInputValue {
+  type Error = LoaderError;
+  fn try_from(input: InputValue<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
+      name: input.name,
+      description: input.description,
+      value_type: input.value_type.try_into()?,
+      default_value: input.default_value.map(graphql_to_json).transpose()?,
+      directives: graphql_directives_to_object(input.directives)?,
+    })
+  }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize)]
+pub struct GraphqlType {
+  pub name: Option<String>,
+  pub required: bool,
+  #[serde(rename = "listOf")]
+  pub list_of: Option<Box<GraphqlType>>,
+}
+
+impl<'a> TryFrom<Type<'a, String>> for GraphqlType {
+  type Error = LoaderError;
+  fn try_from(typ: Type<'a, String>) -> Result<Self, Self::Error> {
+    Ok(match typ {
+      Type::NamedType(s) => Self {
+        name: Some(s),
+        required: false,
+        list_of: None,
+      },
+      Type::ListType(b) => {
+        let t: GraphqlType = (*b).try_into()?;
+        Self {
+          name: None,
+          required: true,
+          list_of: Some(Box::new(t)),
+        }
+      }
+      Type::NonNullType(b) => {
+        let t: GraphqlType = (*b).try_into()?;
+        Self { required: true, ..t }
+      }
+    })
+  }
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct GraphqlEnumValue {
+  pub name: String,
+  pub description: Option<String>,
+  pub directives: Directives,
+}
+
+impl<'a> TryFrom<EnumValue<'a, String>> for GraphqlEnumValue {
+  type Error = LoaderError;
+  fn try_from(value: EnumValue<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
+      name: value.name,
+      description: value.description,
+      directives: graphql_directives_to_object(value.directives)?,
+    })
+  }
+}
+
+#[derive(Debug, Copy, Clone, Serialize)]
 pub enum GraphqlDefinitionType {
   Scalar,
   Object,
@@ -58,64 +168,116 @@ pub enum GraphqlDefinitionType {
   InputObject,
 }
 
-impl<'a> From<TypeDefinition<'a, String>> for GraphqlDefinition {
-  fn from(type_def: TypeDefinition<'a, String>) -> Self {
-    let def: Self = match type_def {
-      TypeDefinition::Scalar(value) => value.into(),
-      TypeDefinition::Object(value) => value.into(),
-      TypeDefinition::Interface(value) => value.into(),
-      TypeDefinition::Union(value) => value.into(),
-      TypeDefinition::Enum(value) => value.into(),
-      TypeDefinition::InputObject(value) => value.into(),
-    };
-    def
+impl<'a> TryFrom<TypeDefinition<'a, String>> for GraphqlDefinition {
+  type Error = LoaderError;
+  fn try_from(type_def: TypeDefinition<'a, String>) -> Result<Self, Self::Error> {
+    match type_def {
+      TypeDefinition::Scalar(value) => value.try_into(),
+      TypeDefinition::Object(value) => value.try_into(),
+      TypeDefinition::Interface(value) => value.try_into(),
+      TypeDefinition::Union(value) => value.try_into(),
+      TypeDefinition::Enum(value) => value.try_into(),
+      TypeDefinition::InputObject(value) => value.try_into(),
+    }
   }
 }
 
-impl<'a> From<ScalarType<'a, String>> for GraphqlDefinition {
-  fn from(type_def: ScalarType<'a, String>) -> Self {
-    Self {
+impl GraphqlDefinition {}
+
+impl<'a> TryFrom<ScalarType<'a, String>> for GraphqlDefinition {
+  type Error = LoaderError;
+  fn try_from(def: ScalarType<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
       definition_type: GraphqlDefinitionType::Scalar,
-    }
+      name: def.name,
+      description: def.description,
+      directives: graphql_directives_to_object(def.directives)?,
+      fields: vec![],
+      implements_interfaces: vec![],
+      union_of: vec![],
+      values: vec![],
+      inputs: vec![],
+    })
   }
 }
-
-impl<'a> From<ObjectType<'a, String>> for GraphqlDefinition {
-  fn from(type_def: ObjectType<'a, String>) -> Self {
-    Self {
+impl<'a> TryFrom<ObjectType<'a, String>> for GraphqlDefinition {
+  type Error = LoaderError;
+  fn try_from(def: ObjectType<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
       definition_type: GraphqlDefinitionType::Object,
-    }
+      name: def.name,
+      description: def.description,
+      directives: graphql_directives_to_object(def.directives)?,
+      fields: def.fields.into_iter().map(|f| f.try_into()).collect::<Result<Vec<_>, _>>()?,
+      implements_interfaces: def.implements_interfaces,
+      union_of: vec![],
+      values: vec![],
+      inputs: vec![],
+    })
   }
 }
-
-impl<'a> From<InterfaceType<'a, String>> for GraphqlDefinition {
-  fn from(type_def: InterfaceType<'a, String>) -> Self {
-    Self {
+impl<'a> TryFrom<InterfaceType<'a, String>> for GraphqlDefinition {
+  type Error = LoaderError;
+  fn try_from(def: InterfaceType<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
       definition_type: GraphqlDefinitionType::Interface,
-    }
+      name: def.name,
+      description: def.description,
+      directives: graphql_directives_to_object(def.directives)?,
+      fields: def.fields.into_iter().map(|f| f.try_into()).collect::<Result<Vec<_>, _>>()?,
+      implements_interfaces: def.implements_interfaces,
+      union_of: vec![],
+      values: vec![],
+      inputs: vec![],
+    })
   }
 }
-
-impl<'a> From<UnionType<'a, String>> for GraphqlDefinition {
-  fn from(type_def: UnionType<'a, String>) -> Self {
-    Self {
+impl<'a> TryFrom<UnionType<'a, String>> for GraphqlDefinition {
+  type Error = LoaderError;
+  fn try_from(def: UnionType<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
       definition_type: GraphqlDefinitionType::Union,
-    }
+      name: def.name,
+      description: def.description,
+      directives: graphql_directives_to_object(def.directives)?,
+      implements_interfaces: vec![],
+      fields: vec![],
+      union_of: def.types,
+      values: vec![],
+      inputs: vec![],
+    })
   }
 }
-
-impl<'a> From<EnumType<'a, String>> for GraphqlDefinition {
-  fn from(type_def: EnumType<'a, String>) -> Self {
-    Self {
+impl<'a> TryFrom<EnumType<'a, String>> for GraphqlDefinition {
+  type Error = LoaderError;
+  fn try_from(def: EnumType<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
       definition_type: GraphqlDefinitionType::Enum,
-    }
+      name: def.name,
+      description: def.description,
+      directives: graphql_directives_to_object(def.directives)?,
+      fields: vec![],
+      implements_interfaces: vec![],
+      union_of: vec![],
+      values: def.values.into_iter().map(|v| v.try_into()).collect::<Result<Vec<_>, _>>()?,
+      inputs: vec![],
+    })
   }
 }
-impl<'a> From<InputObjectType<'a, String>> for GraphqlDefinition {
-  fn from(type_def: InputObjectType<'a, String>) -> Self {
-    Self {
+impl<'a> TryFrom<InputObjectType<'a, String>> for GraphqlDefinition {
+  type Error = LoaderError;
+  fn try_from(def: InputObjectType<'a, String>) -> Result<Self, Self::Error> {
+    Ok(Self {
       definition_type: GraphqlDefinitionType::InputObject,
-    }
+      name: def.name,
+      description: def.description,
+      directives: graphql_directives_to_object(def.directives)?,
+      inputs: def.fields.into_iter().map(|f| f.try_into()).collect::<Result<Vec<_>, _>>()?,
+      implements_interfaces: vec![],
+      fields: vec![],
+      union_of: vec![],
+      values: vec![],
+    })
   }
 }
 
@@ -131,7 +293,7 @@ pub fn graphql_to_object(graphql: Document<String>) -> Result<Graphql, LoaderErr
         schema.subscription = schema_def.subscription;
         schema.directives = graphql_directives_to_object(schema_def.directives)?
       }
-      Definition::TypeDefinition(type_def) => definitions.push(type_def.into()),
+      Definition::TypeDefinition(type_def) => definitions.push(type_def.try_into()?),
       Definition::TypeExtension(_) => {
         // ignored
       }
@@ -187,20 +349,6 @@ mod test {
 
     let obj = graphql_to_object(ast)?;
     dbg!(obj);
-
-    Ok(())
-  }
-
-  #[test]
-  fn read_some_openapi() -> Result<(), anyhow::Error> {
-    use openapiv3::OpenAPI;
-
-    let mut file = File::open("./_samples/loader/petstore.openapi3.yaml")?;
-    let mut content = String::new();
-    file.read_to_string(&mut content)?;
-
-    let openapi: OpenAPI = serde_yaml::from_str(&content).expect("Could not deserialize input");
-    println!("{:?}", openapi);
 
     Ok(())
   }
